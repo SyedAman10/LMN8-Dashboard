a5d6c3e171f63a735e781ffa102a61ae9e4268d8
SyedAman10 <amanullahnaqvi@gmail.com>
2026-05-22 00:07:33 +0500

    A

--- src/scripts/reset-db.js
+++ src/scripts/reset-db.js
@@ -6,6 +6,7 @@ import { query, testConnection } from '../lib/db.js';
 async function resetDatabase() {
   console.log('🔄 Resetting database...');
 
+  
   try { 
     // Enable UUID generation for primary keys
     await query('CREATE EXTENSION IF NOT EXISTS pgcrypto');
